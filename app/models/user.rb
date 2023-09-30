a6bc40c55135f6744c2317b3d90e7b6b0cd83868
emarkees <rphillipse@gmail.com>
2023-09-30 17:22:37 -0600

    Fix linters error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
   validates_presence_of :password
   validates_presence_of :name
 end
